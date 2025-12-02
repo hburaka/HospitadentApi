79ba2b848ce283a78258d9410feadbd1315c8f80
Yasin Gülsoy <yasingulsoy02@gmail.com>
2025-12-02 14:57:52 +0300

    Add DotNetEnv package and load environment variables in Program.cs

--- HospitadentApi.WebService/Program.cs
+++ HospitadentApi.WebService/Program.cs
@@ -1,3 +1,8 @@
+using DotNetEnv;
+
+// .env dosyasını (varsa) yükle
+Env.Load(); // Varsayılan olarak çalışma dizinindeki .env dosyasını okur
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
