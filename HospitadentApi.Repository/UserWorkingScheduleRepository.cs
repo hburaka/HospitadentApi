07a22065f337d7a72f08fdbf3f4d41729832e4c6
hburaka <hburaka@gmail.com>
2025-12-08 11:19:27 +0300

    kaldırma

--- HospitadentApi.Repository/UserWorkingScheduleRepository.cs
+++ HospitadentApi.Repository/UserWorkingScheduleRepository.cs
@@ -1,9 +1,4 @@
-// HospitadentApi.Repository\UserWorkingScheduleRepository.cs
 using HospitadentApi.Entity;
-using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Logging;
 
